5242d0c4d64e8e2078ef63fce0cf5a3ed56905c4
nishantaggarwal <naggarwal@mafcarrefour.com>
2025-04-27 01:09:45 +0530

    removeKthElementDLL

--- src/main/java/org/example/doublylinkedlist/DoublyLinkedListDriver.java
+++ src/main/java/org/example/doublylinkedlist/DoublyLinkedListDriver.java
@@ -9,7 +9,7 @@ public class DoublyLinkedListDriver {
         Node head = DoublyLinkedListUtils.convertArrayToDLL(arr);
         System.out.println("Printing the DLL:");
         DoublyLinkedListUtils.printDLL(head);
-        head= DoublyLinkedListUtils.deleteHead(head);
+        head = DoublyLinkedListUtils.removeKthElementDLL(head,6);
         System.out.println("Printing the DLL:");
         DoublyLinkedListUtils.printDLL(head);
 
--- src/main/java/org/example/doublylinkedlist/DoublyLinkedListUtils.java
+++ src/main/java/org/example/doublylinkedlist/DoublyLinkedListUtils.java
@@ -24,6 +24,52 @@ public class DoublyLinkedListUtils {
         return head;
     }
 
+    public static Node deleteTail(Node head) {
+        if (head == null || head.next == null) return null;
+
+       Node tail = head;
+
+       while (tail.next!=null)
+       {
+           tail = tail.next;
+       }
+
+       Node prev = tail.back;
+       prev.next=null;
+       tail.back=null;
+       return head;
+    }
+
+    public static Node removeKthElementDLL(Node head, int k) {
+        if (head == null) return null;
+
+        int ctr=0;
+        Node kthNode = head;
+        while (kthNode!=null) {
+            ctr++;
+            if(ctr==k) break;;
+            kthNode = kthNode.next;
+        }
+
+        if(ctr==k) {
+            Node prev = kthNode.back;
+            Node front = kthNode.next;
+
+            if (prev == null && front == null) {
+                return null;
+            } else if (prev == null) {
+                return deleteHead(head);
+            } else if (front == null) {
+                return deleteTail(head);
+            }
+            prev.next = front;
+            front.back = prev;
+            kthNode.next = null;
+            kthNode.back = null;
+        }
+        return head;
+    }
+
     public static void printDLL(Node head){
         Node current = head;
         while (current!=null)
